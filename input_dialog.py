6566f07c2521a4d5e2953b2fef78c477ae088365
vigilantPotato <vigilant.potato@gmail.com>
2023-09-25 23:05:20 +0900

    change design

--- input_dialog.py
+++ input_dialog.py
@@ -13,45 +13,43 @@ class InputDialog(tkinter.Toplevel):
     def __init__(self, root, default_title=None, default_url=None):
         super().__init__(root)
         self.root = root
-
-        frame1 = tkinter.Frame(self)
-        frame1.pack(padx=5, pady=5)
+        self.var = tkinter.StringVar(root)
 
         #demo button
-        label = tkinter.Label(frame1, text="Demo : ")
-        label.grid(row=0, column=0)
-        self.var = tkinter.StringVar(root)
-        demo_button = TestButton(frame1, self.var)
-        demo_button.grid(row=0, column=1, sticky="w")
+        demo_frame = tkinter.LabelFrame(self, text="Demo button")
+        demo_frame.pack()
+        self.demo_button = TestButton(demo_frame, self.var)
+        self.demo_button.pack(padx=10, pady=10)
+
+        #input information frame
+        input_frame = tkinter.LabelFrame(self, text="Input button information")
+        input_frame.pack(padx=5, pady=5)
 
         #title
-        self.attributes("-topmost", True)
         self.title("Input information")
-        label = tkinter.Label(frame1, text="Title : ")
-        label.grid(row=1, column=0)
-        self.entry = tkinter.Entry(frame1, width=30, textvariable=self.var)
+        label = tkinter.Label(input_frame, text="Title : ")
+        label.grid(row=0, column=0)
+        self.entry = tkinter.Entry(input_frame, width=30, textvariable=self.var)
         if default_title:
             self.entry.insert(0, default_title)
-        self.entry.grid(row=1, column=1, sticky="w")
+        self.entry.grid(row=0, column=1, sticky="w")
 
         #url
-        label = tkinter.Label(frame1, text="URL : ")
-        label.grid(row=2, column=0)
-        self.entry2 = tkinter.Entry(frame1, width=50)
+        label = tkinter.Label(input_frame, text="URL : ")
+        label.grid(row=1, column=0)
+        self.entry2 = tkinter.Entry(input_frame, width=50)
         if default_url:
             self.entry2.insert(0, default_url)
-        self.entry2.grid(row=2, column=1)
+        self.entry2.grid(row=1, column=1)
 
         #color setting
-        label = tkinter.Label(frame1, text="Background Color: ")
-        label.grid(row=3, column=0)
-        self.color_select = ColorSelectButton(frame1, text="background", button=demo_button)
-        self.color_select.grid(row=3, column=1, sticky="w")
+        color_setting_frame = tkinter.LabelFrame(self, text="Button color setting")
+        color_setting_frame.pack()
+        self.color_select = ColorSelectButton(color_setting_frame, text="background", button=self.demo_button)
+        self.color_select.pack(side=tkinter.LEFT)
 
-        label = tkinter.Label(frame1, text="Text Color: ")
-        label.grid(row=4, column=0)
-        self.color_select = ColorSelectButton(frame1, text="text color", button=demo_button)
-        self.color_select.grid(row=4, column=1, sticky="w")
+        self.color_select = ColorSelectButton(color_setting_frame, text="text color", button=self.demo_button)
+        self.color_select.pack(side=tkinter.LEFT)
 
         frame2 = tkinter.Frame(self)
         frame2.pack(padx=5, pady=5)
@@ -85,7 +83,7 @@ class InputDialog(tkinter.Toplevel):
         self.wait_window()
     
     def finished(self):
-        self.result = self.entry.get(), self.entry2.get()
+        self.result = self.entry.get(), self.entry2.get(), self.demo_button["bg"], self.demo_button["fg"]
         self.destroy()
     
     def canceled(self):
